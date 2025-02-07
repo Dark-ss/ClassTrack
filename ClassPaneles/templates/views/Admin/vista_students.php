cd6d8c81b9da02e693d7c3ba016845722a8931e8
Diaz <nicolleydiazm@gmail.com>
2025-02-07 11:32:59 -0500

    Update vista_students.php

--- ClassPaneles/templates/views/Admin/vista_students.php
+++ ClassPaneles/templates/views/Admin/vista_students.php
@@ -58,7 +58,7 @@ if (isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ver Estudiantes</title>
-    <link rel="stylesheet" href="../../assets/css/style_paneles.css">
+    <link rel="stylesheet" href="../../assets/css/style_panel.css">
 </head>
 
 <body>
